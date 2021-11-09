a417e1b8938254fadd199330f0b7b61ef4cf0c29
Gabriel B. Sant'Anna <baiocchi.gabriel@gmail.com>
2021-11-09 17:54:47 -0300

    Fix fisheye and DDA rounding

--- src/Game.fs
+++ src/Game.fs
@@ -12,11 +12,11 @@ open Engine
 
 type CanvasRenderingContext2D with
     /// Draws a single, vertically centered, 1px-wide column.
-    member this.drawColumn(x: int, columnHeight: int) =
+    member this.drawColumn(column: int, columnHeight: int) =
         this.lineWidth <- 1.0
         this.beginPath()
-        this.moveTo(x, this.height/2 - columnHeight/2)
-        this.lineTo(x, this.height/2 + columnHeight/2)
+        this.moveTo(column, this.height/2 - columnHeight/2)
+        this.lineTo(column, this.height/2 + columnHeight/2)
         this.stroke()
 
 
@@ -42,8 +42,8 @@ type Player =
     { X: float; Y: float
       Dir: float<rad> }
 
-    static member SPEED = 32<1/s>
-    static member TURN = 2<rad/s>
+    static member SPEED = 32.0<1/s>
+    static member TURN = 2.0<rad/s>
     static member FOV = deg.toRad 90.0<deg>
 
 type World =
@@ -73,23 +73,27 @@ let inline distance x1 y1 x2 y2 =
 
 
 let init () =
-    let x = 2.5 * float Tile.SIZE
-    let y = 2.5 * float Tile.SIZE
+    let x = 4.5 * float Tile.SIZE
+    let y = 4.5 * float Tile.SIZE
     let o = Empty
     let W = Wall (rgb 0x41 0x41 0x41)
     let D = Wall (rgb 0x3a 0x82 0x83)
     { Map =
         Map.make
-            (16, 9)
-            [| W; W; W; W; W; W; o; o; o; W; o; o; o; o; W; o
-               W; o; o; o; o; o; o; o; o; W; o; o; o; o; W; o
-               D; o; o; o; o; o; o; o; W; D; W; W; W; W; D; W
-               W; o; o; o; o; o; o; o; o; W; o; o; o; o; W; o
-               W; W; W; W; W; W; o; o; o; W; o; o; o; o; W; o
-               W; o; o; o; o; o; o; o; o; W; o; o; o; o; W; o
-               W; o; o; o; o; o; o; o; W; D; W; W; W; W; D; W
-               W; o; o; o; o; o; o; o; o; W; o; o; o; o; W; o
-               W; o; o; o; o; o; o; o; o; W; o; o; o; o; W; o |]
+            (21, 13)
+            [| W; W; W; W; W; W; W; W; W; W; W; W; W; W; W; W; W; W; W; W; W
+               W; o; o; o; o; o; o; o; o; o; o; o; o; o; o; o; o; o; o; o; W
+               W; o; W; W; W; W; W; W; o; o; o; o; W; o; o; o; W; o; o; o; W
+               W; o; W; o; o; o; o; o; o; o; o; o; W; o; o; o; W; o; o; o; W
+               W; o; D; o; o; o; o; o; o; o; W; W; D; W; o; W; D; W; W; o; W
+               W; o; W; o; o; o; o; o; o; o; o; o; W; o; o; o; W; o; o; o; W
+               W; o; W; W; W; W; W; W; o; o; o; o; o; o; o; o; o; o; o; o; W
+               W; o; W; o; o; o; o; o; o; o; o; o; W; o; o; o; W; o; o; o; W
+               W; o; W; o; o; o; o; o; o; o; W; W; D; W; o; W; D; W; W; o; W
+               W; o; W; o; o; o; o; o; o; o; o; o; W; o; o; o; W; o; o; o; W
+               W; o; W; o; o; o; o; o; o; o; o; o; W; o; o; o; W; o; o; o; W
+               W; o; o; o; o; o; o; o; o; o; o; o; o; o; o; o; o; o; o; o; W
+               W; W; W; W; W; W; W; W; W; W; W; W; W; W; W; W; W; W; W; W; W |]
       Player = { X = x; Y = y; Dir = 0.0<rad> } },
     Cmd.none
 
@@ -100,25 +104,24 @@ let update dt game =
     let map = game.State.Map
 
     // joystick-style controls: move forwards/backwards and steer left/right
-    let speed = 0<1/s>
+    let speed = 0.0<1/s>
     let speed = if isKeyPressed "ArrowUp" || isKeyPressed "KeyW" then speed + Player.SPEED else speed
     let speed = if isKeyPressed "ArrowDown" || isKeyPressed "KeyS" then speed - Player.SPEED else speed
-    let speed = if speed < 0<1/s> then speed / 2 else speed
-    let steer = 0<rad/s>
+    let speed = if speed < 0.0<1/s> then speed*0.5 else speed
+    let steer = 0.0<rad/s>
     let steer = if isKeyPressed "ArrowLeft" || isKeyPressed "KeyA" then steer - Player.TURN else steer
     let steer = if isKeyPressed "ArrowRight" || isKeyPressed "KeyD" then steer + Player.TURN else steer
 
     // scale by timestep
-    let dist = f speed / ms.perSecond * dt
-    let turn = f steer / ms.perSecond * dt
+    let dist = speed / ms.perSecond * dt
+    let turn = steer / ms.perSecond * dt
 
     // ad-hoc movement
     let dir = player.Dir + turn
     let dir = dir % (2.0<rad>*Math.PI)
     let x, y = step player.X player.Y dist dir
-    let x, y =
-        Math.Clamp(x, 0.0, float ((map.Width - 1) * Tile.SIZE)),
-        Math.Clamp(y, 0.0, float ((map.Height - 1) * Tile.SIZE))
+    let x = Math.Clamp(x, 0.0, float <| (map.Width - 1)  * Tile.SIZE)
+    let y = Math.Clamp(y, 0.0, float <| (map.Height - 1) * Tile.SIZE)
 
     // check for collisions
     let x, y =
@@ -141,7 +144,7 @@ let handler event game =
 let draw state (ctx: Canvas) =
     let w, h = ctx.width, ctx.height
     let player = state.Player
-    let px, py, dir = int player.X, int player.Y, player.Dir
+    let px, py, dir = player.X, player.Y, player.Dir
     let fov = Player.FOV
     let map = state.Map
 
@@ -157,7 +160,7 @@ let draw state (ctx: Canvas) =
         let angle =
             let radPerPixel = fov / f (w * 1<px>)
             let pixelsFromCenter = (column - w/2) * 1<px>
-            dir + f pixelsFromCenter * radPerPixel
+            dir + radPerPixel * (f pixelsFromCenter)
         let angle = angle % (2.0<rad>*Math.PI)
 
         // to find the first grid-aligned intersections, we solve the system
@@ -165,62 +168,59 @@ let draw state (ctx: Canvas) =
         // then y' = py + m * (x' - px) and x' = px + (1/m) * (y' - py)
         let dx, dy =
             let tx, ty = step px py (2*Tile.SIZE) angle
-            int tx - px, int ty - py
+            tx - px, ty - py
         let intersectX =
-            if dx = 0 then
-                None
-            else
-                let x' = px - px % Tile.SIZE + (if dx > 0 then Tile.SIZE else 0)
-                let y' = py + dy * (x' - px) / dx
-                Some (x', y')
+            let x' = px - (px % float Tile.SIZE) + (if dx > 0.0 then float Tile.SIZE else 0.0)
+            let y' = py + (dy/dx) * (x' - px)
+            x', y'
         let intersectY =
-            if dy = 0 then
-                None
-            else
-                let y' = py - py % Tile.SIZE + (if dy > 0 then Tile.SIZE else 0)
-                let x' = px + dx * (y' - py) / dy
-                Some (x', y')
+            let y' = py - (py % float Tile.SIZE) + (if dy > 0.0 then float Tile.SIZE else 0.0)
+            let x' = px + (dx/dy) * (y' - py)
+            x', y'
 
         // march the ray forward until something is hit (or map limit reached)
-        let stepY = int <| float Tile.SIZE * Math.Tan(float angle)
-        let stepY = Math.Abs(stepY) * Math.Sign(dy)
-        let stepX = int <| float Tile.SIZE * Math.Tan(Math.PI / 2.0 - float angle)
-        let stepX = Math.Abs(stepX) * Math.Sign(dx)
+        let gridX, gridY = float (Math.Sign(dx)*Tile.SIZE), float (Math.Sign(dy)*Tile.SIZE)
+        let stepY = float Tile.SIZE * Math.Tan(float angle)
+        let stepY = Math.Abs(stepY) * float (Math.Sign(dy))
+        let stepX = float Tile.SIZE * Math.Tan(Math.PI/2.0 - float angle)
+        let stepX = Math.Abs(stepX) * float (Math.Sign(dx))
         let rec rayMarch x y intersectX intersectY =
+            // move to the closest grid intersection point
+            let distX = distance x y <|| intersectX
+            let distY = distance x y <|| intersectY
+            let x, y, intersectX, intersectY, dir =
+                if distX < distY then
+                    let x', y' = intersectX
+                    x', y', (x' + gridX, y' + stepY), intersectY, Choice1Of2 ()
+                else
+                    let x', y' = intersectY
+                    x', y', intersectX, (x' + stepX, y' + gridY), Choice2Of2 ()
+            // check for a hit, rounding based on the step direction
             let i, j =
-                y / Tile.SIZE + (if y % Tile.SIZE = 0 && dy < 0 then -1 else 0),
-                x / Tile.SIZE + (if x % Tile.SIZE = 0 && dx < 0 then -1 else 0)
+                let x, y = int x, int y
+                match dir with
+                | Choice1Of2 () -> y / Tile.SIZE, x / Tile.SIZE + (if dx < 0.0 then -1 else 0)
+                | Choice2Of2 () -> y / Tile.SIZE + (if dy < 0.0 then -1 else 0), x / Tile.SIZE
             if j < 0 || j >= map.Width || i < 0 || i >= map.Height then
                 None
             else
                 match map.[i, j] with
-                | Wall _ as w -> Some (w, x, y)
-                | Empty -> // move to the closest grid intersection point
-                    let distX =
-                        if Option.isSome intersectX then
-                            distance x y <|| Option.get intersectX
-                        else
-                            infinity
-                    let distY =
-                        if Option.isSome intersectY then
-                            distance x y <|| Option.get intersectY
-                        else
-                            infinity
-                    if distX <= distY then
-                        let x', y' = Option.get intersectX
-                        rayMarch x' y' (Some (x' + Math.Sign(dx)*Tile.SIZE, y' + stepY)) intersectY
-                    else
-                        let x', y' = Option.get intersectY
-                        rayMarch x' y' intersectX (Some (x' + stepX, y' + Math.Sign(dy)*Tile.SIZE))
+                | Wall _ as w -> Some ((x, y), (i, j))
+                | Empty -> rayMarch x y intersectX intersectY
         let hit = rayMarch px py intersectX intersectY
 
         // draw a single vertical column based on what was hit
         match hit with
-        | Some (Wall color, hitX, hitY) ->
-            let height = 2048.0 / distance px py hitX hitY
-            ctx.color <- color
-            ctx.drawColumn(column, int height)
-        | None | Some (Empty, _, _) -> ()
+        | Some (position, tile) ->
+            match map.[tile] with
+            | Empty -> ()
+            | Wall color ->
+                let distance = distance px py <|| position
+                let fishEyeCorrected = distance * cos (float (dir - angle))
+                let height = 2048.0 / fishEyeCorrected
+                ctx.color <- color
+                ctx.drawColumn(column, int height)
+        | None -> ()
 
     // minimap
     ctx.save()
@@ -237,10 +237,10 @@ let draw state (ctx: Canvas) =
 
     // "miniplayer"
     ctx.color <- rgba 0 255 0 0.5
-    ctx.fillRect(px, py, 4, 4)
+    ctx.fillRect(px, py, 4.0, 4.0)
     ctx.beginPath()
     ctx.moveTo(px, py)
-    let tx, ty = step px py 16.0 dir in ctx.lineTo(int tx, int ty)
+    let tx, ty = step px py 16.0 dir in ctx.lineTo(tx, ty)
     ctx.stroke()
 
 
